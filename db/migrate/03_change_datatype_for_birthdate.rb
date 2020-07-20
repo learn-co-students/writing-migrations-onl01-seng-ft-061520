a7c19346ae343e6093a20ebd296520ab50f6f1a3
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-20 19:47:10 +0000

    Done.

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,8 +1,8 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
-
+binding.pry
   def change 
   
-  change_column :students, :datetime, :birthdate
+  change_column :students, :birthdate, :datetime
   
   end 
 
