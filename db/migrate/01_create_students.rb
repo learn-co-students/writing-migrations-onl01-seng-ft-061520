1536121b45ba2e71431a733faf6e0ec42cc8e83e
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-07-20 11:06:25 -0600

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,7 +2,6 @@ class CreateStudents < ActiveRecord::Migration[5.2]
     def change
         create_table :students do |t|
           t.string :name 
-          t.string :type
         end
       end
 end
